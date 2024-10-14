3daeaa3ede724065aa99cb1dc4d5aa8d25caea05
luohao <365624786@qq.com>
2024-10-14 09:47:44 +0800

    商品详情接口

--- MvvmRoot/src/main/java/com/zyxcoder/mvvmroot/base/adapter/BaseViewBindingHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.zyxcoder.mvvmroot.base.adapter
-
-import androidx.viewbinding.ViewBinding
-import com.chad.library.adapter.base.viewholder.BaseViewHolder
-
-/**
- * Create by zyx_coder on 2023/1/9
- */
-class BaseViewBindingHolder<VB : ViewBinding>(var viewBind: VB) : BaseViewHolder(viewBind.root)
--- MvvmRoot/src/main/java/com/zyxcoder/mvvmroot/network/manager/NetState.kt
+++
@@ -1,8 +0,0 @@
-package com.zyxcoder.mvvmroot.network.manager
-
-/**
- * Create by zyx_coder on 2022/11/17
- */
-class NetState(
-    var isSuccess: Boolean = true
-)
